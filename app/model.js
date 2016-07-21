25baa4bfc0446a3c667be7bb74a7f30be6afc9cf
Nuno Miranda <nmiranda@int.viatecla.pt>
2016-07-21 23:21:16 +0100

    new data model

--- app/model.js
+++ app/model.js
@@ -3,19 +3,17 @@ var mongoose    = require('mongoose');
 var Schema      = mongoose.Schema;
 
 // Creates a User Schema. This will be the basis of how user data is stored in the db
-var UserSchema = new Schema({
-    username: {type: String, required: true},
-    gender: {type: String, required: true},
-    age: {type: Number, required: true},
-    favlang: {type: String, required: true},
+var FestivalSchema = new Schema({
+    festivalName: {type: String, required: true},       
     location: {type: [Number], required: true}, // [Long, Lat]
-    htmlverified: String,
     created_at: {type: Date, default: Date.now},
-    updated_at: {type: Date, default: Date.now}
+    updated_at: {type: Date, default: Date.now},
+	startDate: {type: Date, required: true},
+	endDate:  {type: Date, required: true}
 });
 
 // Sets the created_at parameter equal to the current time
-UserSchema.pre('save', function(next){
+FestivalSchema.pre('save', function(next){
     now = new Date();
     this.updated_at = now;
     if(!this.created_at) {
@@ -25,7 +23,7 @@ UserSchema.pre('save', function(next){
 });
 
 // Indexes this schema in geoJSON format (critical for running proximity searches)
-UserSchema.index({location: '2dsphere'});
+FestivalSchema.index({location: '2dsphere'});
 
-// Exports the UserSchema for use elsewhere. Sets the MongoDB collection to be used as: "scotch-user"
-module.exports = mongoose.model('festivals', UserSchema);
+// Exports the FestivalSchema for use elsewhere. Sets the MongoDB collection to be used as: "scotch-user"
+module.exports = mongoose.model('festivals', FestivalSchema);
--- public/js/addCtrl.js
+++ public/js/addCtrl.js
@@ -40,7 +40,6 @@ addCtrl.controller('addCtrl', function($scope, $http, $rootScope, geolocation, g
         $scope.$apply(function(){
             $scope.formData.latitude = parseFloat(gservice.clickLat).toFixed(3);
             $scope.formData.longitude = parseFloat(gservice.clickLong).toFixed(3);
-            $scope.formData.htmlverified = "Nope";
         });
     });
 
@@ -52,35 +51,34 @@ addCtrl.controller('addCtrl', function($scope, $http, $rootScope, geolocation, g
 
             $scope.formData.longitude = parseFloat(coords.long).toFixed(3);
             $scope.formData.latitude = parseFloat(coords.lat).toFixed(3);
-            $scope.formData.htmlverified = "Yep (Thanks for giving us real data!)";
             
             gservice.refresh(coords.lat, coords.long);
             
         });
     };
-
+	
+	
     // Creates a new user based on the form fields
     $scope.createUser = function() {
-
+		var concatStartDate = $scope.formData.startDate +"T"+ $scope.formData.startDateTime;
+		var concatEndDate = $scope.formData.endDate +"T"+ $scope.formData.endDateTime;	
+	
         // Grabs all of the text box fields
-        var userData = {
-            username: $scope.formData.username,
-            gender: $scope.formData.gender,
-            age: $scope.formData.age,
-            favlang: $scope.formData.favlang,
-            location: [$scope.formData.longitude, $scope.formData.latitude],
-            htmlverified: $scope.formData.htmlverified
+        var festivalData = {
+            festivalName: $scope.formData.festivalName,
+			location: [$scope.formData.longitude, $scope.formData.latitude],
+			created_at: $scope.formData.created_at,
+			updated_at: $scope.formData.updated_at,
+            startDate: concatStartDate,
+			endDate: concatEndDate
         };
 
         // Saves the user data to the db
-        $http.post('/users', userData)
+        $http.post('/users', festivalData)
             .success(function (data) {
 
                 // Once complete, clear the form (except location)
-                $scope.formData.username = "";
-                $scope.formData.gender = "";
-                $scope.formData.age = "";
-                $scope.formData.favlang = "";
+                $scope.formData.festivalName = "";
 
                 // Refresh the map with new data
                 gservice.refresh($scope.formData.latitude, $scope.formData.longitude);
